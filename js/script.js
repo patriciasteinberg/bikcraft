b7889fafb90fd290e04c4ed715ad2b42cd1afd7b
Patricia Steinberg <patriciacsteinberg@gmail.com>
2020-12-20 23:24:16 -0500

    project finished

---
+++ js/script.js
@@ -0,0 +1,25 @@
+if(window.SimpleSlide){
+    new SimpleSlide({
+        slide: "quote",
+        time: 5000
+    });
+
+    new SimpleSlide({
+        slide: "portfolio",
+        time: 5000,
+        nav: true
+    });
+}
+
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
+
+if(window.SimpleForm) {
+    new SimpleForm({
+        form: ".formphp",
+        button: "#enviar",
+        erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o e-mail contato@bikcraft.com.</p></div>",
+        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso.</h2><p>Em breve, eu entro em contato.</p></div>"
+    });
+}
